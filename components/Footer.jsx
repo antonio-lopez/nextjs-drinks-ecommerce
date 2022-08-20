a3b8d2ac2bc62053259bc1ebb86f40a417eab093
Antonio Lopez <pezlop202@gmail.com>
2022-08-20 14:53:53 -0700

    Fixed Footer rounded links

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -8,31 +8,31 @@ const Footer = () => {
         <span className="text-xl">Follow</span>
         <div className="flex space-x-3">
           <a
-            className="rounded-full p-2 outline outline-1 hover:text-white/70"
+            className="hover:text-white/70"
             href="https://github.com/antonio-lopez"
             aria-label="Antonio Lopez github profile"
             target="_blank"
             rel="noopener noreferrer"
           >
-            <AiOutlineGithub className="h-6 w-6" />
+            <AiOutlineGithub className="h-9 w-9" />
           </a>
           <a
-            className="rounded-full p-2 outline outline-1 hover:text-white/70"
+            className="hover:text-white/70"
             href="https://www.linkedin.com/in/antoniolopez6/"
             aria-label="Antonio Lopez linkedin profile"
             target="_blank"
             rel="noopener noreferrer"
           >
-            <AiOutlineLinkedin className="h-6 w-6" />
+            <AiOutlineLinkedin className="h-9 w-9" />
           </a>
           <a
-            className="rounded-full p-2 outline outline-1 hover:text-white/70"
+            className="hover:text-white/70"
             href="https://www.antoniolopez.me/"
             aria-label="Antonio Lopez website"
             target="_blank"
             rel="noopener noreferrer"
           >
-            <BsGlobe className="h-6 w-6" />
+            <BsGlobe className="h-8 w-8" />
           </a>
         </div>
         <p className="pt-14 text-sm">@2022 Antonio Lopez</p>
