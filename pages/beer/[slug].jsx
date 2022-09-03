9b82610444ebfc5a3237b9a6dc7017a71c91d2f0
Antonio Lopez <pezlop202@gmail.com>
2022-09-02 19:04:59 -0700

    Added getStaticPaths and getStaticProps to single Beer item

--- pages/beer/[slug].jsx
+++ pages/beer/[slug].jsx
@@ -1,32 +1,58 @@
 import { ProductDetails } from '../../components';
+import { client, urlFor } from '../../lib/client';
 
-const Beer = () => {
-  const placeholder = {
-    name: 'Fortunate Islands',
-    imgSrc: '../images/beer/sm/FortunateIslands4Pack_480x.webp',
-    imgDesc: 'beer product',
-    style: 'Beer Style: Hoppy Pale Ale',
-    styleInfo: '5.0% ABV • 16 FL OZ',
-    notes: 'Notes: Mango - Tangerine - Passionfruit',
-    qty: 'Quantity : 4 Pack',
-    price: '$12',
-    description:
-      'Fortunate Islands combines the bangin hop aroma of an über-tropical IPA with the crisp, deeply quenching drinkability of a killer pale ale. We achieve this magical result by starting with a mild, nutty malt backbone, then dry-hopping the bejesus out of it with Citra and Amarillo hops. Restrained bitterness and vibrant notes of mango, tangerine, and passionfruit combine to form a sessionable crusher of mind-bending proportions. It tastes like wizards.',
-  };
+const Beer = ({ beer }) => {
+  const {
+    image,
+    name,
+    beerStyle,
+    price,
+    quantity,
+    beerDetails,
+    styleDetails,
+    notes,
+  } = beer;
 
   return (
     <ProductDetails
-      name={placeholder.name}
-      imgSrc={placeholder.imgSrc}
-      imgDesc={placeholder.imgDesc}
-      style={placeholder.style}
-      styleInfo={placeholder.styleInfo}
-      notes={placeholder.notes}
-      qty={placeholder.qty}
-      price={placeholder.price}
-      description={placeholder.description}
+      image={urlFor(image[0])}
+      name={name}
+      style={beerStyle}
+      price={price}
+      size={quantity}
+      description={beerDetails}
+      styleDetails={styleDetails}
+      notes={notes}
     />
   );
+};
+
+export const getStaticPaths = async () => {
+  const query = `*[_type == "beer"]{
+    slug {
+      current
+    }
+  }`;
+
+  const beerProduct = await client.fetch(query);
+
+  const paths = beerProduct.map((beer) => ({
+    params: {
+      slug: beer.slug.current,
+    },
+  }));
+
+  return {
+    paths,
+    fallback: 'blocking',
+  };
+};
+
+export const getStaticProps = async ({ params: { slug } }) => {
+  const query = `*[_type == "beer" && slug.current == '${slug}'][0]`;
+  const beer = await client.fetch(query);
+
+  return { props: { beer } };
 };
 
 export default Beer;
