c99dfc70c8560c81d6fe004426f341043af1c2bb
Antonio Lopez <pezlop202@gmail.com>
2022-09-20 18:21:15 -0700

    Added state context

---
+++ context/StateContext.jsx
@@ -0,0 +1,69 @@
+/* eslint-disable no-param-reassign */
+/* eslint-disable consistent-return */
+/* eslint-disable array-callback-return */
+/* eslint-disable no-underscore-dangle */
+import React, { createContext, useContext, useState } from 'react';
+import { toast } from 'react-hot-toast';
+
+const Context = createContext();
+
+export const StateContext = ({ children }) => {
+  const [cartItems, setCartItems] = useState([]);
+  const [totalPrice, setTotalPrice] = useState(0);
+  const [totalQuantities, setTotalQuantities] = useState(0);
+  const [qty, setQty] = useState(1);
+
+  const onAdd = (product, quantity) => {
+    const checkProductInCart = cartItems.find(
+      (item) => item._id === product._id
+    );
+    setTotalPrice(
+      (prevTotalPrice) => prevTotalPrice + product.price * quantity
+    );
+    setTotalQuantities((prevTotalQuantities) => prevTotalQuantities + quantity);
+
+    if (checkProductInCart) {
+      const updatedCartItems = cartItems.map((cartProduct) => {
+        if (cartProduct._id === product._id)
+          return { ...cartProduct, quantity: cartProduct.quantity + quantity };
+      });
+
+      setCartItems(updatedCartItems);
+    } else {
+      product.quantity = quantity;
+
+      setCartItems([...cartItems, { ...product }]);
+    }
+    toast.success(`${qty} ${product.name} added to the cart.`);
+  };
+
+  const incrementQty = () => {
+    setQty((prevQty) => prevQty + 1);
+  };
+
+  const decrementQty = () => {
+    setQty((prevQty) => {
+      if (prevQty - 1 < 1) return 1;
+
+      return prevQty - 1;
+    });
+  };
+
+  return (
+    <Context.Provider
+      value={{
+        cartItems,
+        totalPrice,
+        totalQuantities,
+        qty,
+        incrementQty,
+        decrementQty,
+        onAdd,
+      }}
+    >
+      {children}
+    </Context.Provider>
+  );
+};
+
+export const useStateContext = () => useContext(Context);
--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,11 +1,16 @@
+import { Toaster } from 'react-hot-toast';
 import Layout from '../components/Layout';
 import '../styles/globals.css';
+import { StateContext } from '../context/StateContext';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <StateContext>
+      <Layout>
+        <Toaster />
+        <Component {...pageProps} />
+      </Layout>
+    </StateContext>
   );
 }
 
